e508ec125d9ca18a9cf4bbc2eb0a0c5788e6b61a
Rohan Reddy <rohanreddy888@gmail.com>
2023-03-30 15:05:25 +0530

    Projects Update & Tweaks

--- app/hero/page.js
+++ app/hero/page.js
@@ -3,28 +3,30 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex justify-start items-center h-screen">
-      <div className="flex flex-col-reverse md:flex-row justify-between items-start md:items-center w-full gap-4">
+    <div className="flex items-center justify-start h-screen">
+      <div className="flex flex-col-reverse items-start justify-between w-full gap-4 md:flex-row md:items-center">
         <div className="flex flex-col gap-2">
           <h1 className="flex flex-col text-3xl font-bolder">Hola 👋 </h1>
-          <h2 className="text-4xl md:text-7xl font-black mt-3">
+          <h2 className="mt-3 text-4xl font-black md:text-7xl">
             I'm Rohan Reddy
           </h2>{" "}
-          <h3 className="text-xl md:text-3xl font-extralight mt-1">
+          <h3 className="mt-1 text-xl md:text-3xl font-extralight">
             UI/UX Engineer & Web3 Entrepreneur
           </h3>
-          <div className="flex flex-row justify-start  items-center space-x-12 w-full mt-4">
+          <div className="flex flex-row items-center justify-start w-full mt-4 space-x-12">
             <div className="flex">
               <Link
                 href="https://twitter.com/rohanreddy888"
                 target="_blank"
-                rel="noreferrer">
+                rel="noreferrer"
+              >
                 <Image
                   className="text-black"
                   src={"/icons/Twitter.svg"}
                   width="40"
                   height="40"
                   alt="Twitter Icon"
+                  priority
                 />
               </Link>
             </div>
@@ -32,13 +34,15 @@ export default function Hero() {
               <Link
                 href="https://www.linkedin.com/in/rohanreddy888/"
                 target="_blank"
-                rel="noreferrer">
+                rel="noreferrer"
+              >
                 <Image
                   className="text-black"
                   src={"/icons/Linkedin.svg"}
                   width="40"
                   height="40"
                   alt="LinkedIn Icon"
+                  priority
                 />
               </Link>
             </div>
@@ -46,13 +50,15 @@ export default function Hero() {
               <Link
                 href="https://www.instagram.com/rohanreddi.eth/"
                 target="_blank"
-                rel="noreferrer">
+                rel="noreferrer"
+              >
                 <Image
                   className="text-black"
                   src={"/icons/Instagram.svg"}
                   width="40"
                   height="40"
                   alt="Instagram Icon"
+                  priority
                 />
               </Link>
             </div>
@@ -60,21 +66,23 @@ export default function Hero() {
               <Link
                 href="https://t.me/rohanreddy8"
                 target="_blank"
-                rel="noreferrer">
+                rel="noreferrer"
+              >
                 <Image
                   className="text-black"
                   src={"/icons/Telegram.svg"}
                   width="40"
                   height="40"
                   alt="Telegram Icon"
+                  priority
                 />
               </Link>
             </div>
           </div>
         </div>
-        <div className="md:mb-0 mb-8 w-56 h-56 md:w-72 md:h-72">
+        <div className="w-56 h-56 mb-8 md:mb-0 md:w-72 md:h-72">
           <Image
-            className="shadow-lg rounded-full p-2 bg-white"
+            className="p-2 bg-white rounded-full shadow-lg"
             src={"/picture.png"}
             width="350"
             height="350"
