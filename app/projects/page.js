ab8106d3a64e43cf4e5607eeedd5dffa1066d2cc
Rohan Reddy <rohanreddy888@gmail.com>
2023-03-30 15:27:28 +0530

    Projects Update & Tweaks

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import Projects from "../data/projects.json";
 export default function Project() {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-start items-start md:items-center w-full py-24">
-        <div className="flex flex-col justify-start items-start w-full">
-          <h1 className="font-black text-4xl">Recent Projects</h1>
+      <div className="flex flex-col items-start justify-start w-full py-24 md:flex-row md:items-center">
+        <div className="flex flex-col items-start justify-start w-full">
+          <h1 className="text-4xl font-black">Recent Projects</h1>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+          <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
             {Projects.map((project, index) => {
               return (
                 <a
@@ -15,22 +16,40 @@ export default function Project() {
                   target="_blank"
                   rel="noreferrer"
                   key={index}
-                  className="bg-white shadow-lg rounded-lg mt-4 md:mt-2">
-                  <Image
-                    className="rounded-lg"
+                  className="mt-4 bg-white rounded-lg shadow-lg md:mt-2"
+                >
+                  <img
+                    className="w-full rounded-lg"
                     src={project.image}
-                    height="200"
+                    height="600"
                     alt={project.name}
                     width="200"
-                    style={{ width: "100%" }}
                   />
                   <div className="p-4 md:px-3 md:py-6">
-                    <h1 className="font-bold text-xl">{project.name}</h1>
+                    <h1 className="text-xl font-bold">{project.name}</h1>
                     <p className="text-base">{project.desc}</p>
                   </div>
                 </a>
               );
             })}
+            <a className="flex items-center justify-center mt-4 bg-white rounded-lg shadow-lg md:mt-2">
+              <Link
+                href="https://github.com/rohanreddy888"
+                target="_blank"
+                rel="noreferrer"
+                className="flex flex-col items-center justify-center gap-2 text-lg font-semibold"
+              >
+                Check out GitHub for more projects
+                <Image
+                  className="text-black"
+                  src={"/icons/Github.png"}
+                  width="40"
+                  height="40"
+                  alt="Telegram Icon"
+                  priority
+                />
+              </Link>
+            </a>
           </div>
         </div>
       </div>
