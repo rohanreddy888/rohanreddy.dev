eab6097a570a28293c16e5f8f622750bebb3f849
Rohan Reddy <rohanreddy888@gmail.com>
2023-08-29 04:48:50 +0530

    Tweet

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "abs.twimg.com" },
+    ],
+  },
 };
 
 module.exports = nextConfig;
